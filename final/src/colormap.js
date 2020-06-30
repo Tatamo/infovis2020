aaa877ab29da9f8ca1f3da56bd0d35c1150e912f
Tatamo <nsorder@gmail.com>
2020-07-01 02:29:58 +0900

    implement diverging color map

---
+++ final/src/colormap.js
@@ -0,0 +1,85 @@
+function Lab2Msh(l, a, b) {
+	const m = Math.sqrt(l * l + a * a + b * b);
+	const s = m !== 0 ? Math.acos(l / m) : 0;
+	const h = Math.atan2(b, a);
+	return [m, s, h];
+}
+
+function Msh2Lab(m, s, h) {
+	const l = m * Math.cos(s);
+	const a = m * Math.sin(s) * Math.cos(h);
+	const b = m * Math.sin(s) * Math.sin(h);
+	return [l, a, b];
+}
+
+function RGB2Msh(r, g, b) {
+	return Lab2Msh(...rgb2lab([r, g, b].map(v => Math.round(v * 255))));
+}
+
+function Msh2RGB(m, s, h) {
+	return lab2rgb(Msh2Lab(m, s, h)).map(v => v / 255);
+}
+
+// https://www.kennethmoreland.com/color-maps/ColorMapsExpanded.pdf
+function interpolateMshColor(msh1, msh2, interp) {
+	const adjustHue = ([m_sat, s_sat, h_sat], m_unsat) => {
+		if (m_sat >= m_unsat) return h_sat;
+		const h_spin = s_sat * Math.sqrt(m_unsat * m_unsat - m_sat * m_sat) / (m_sat * Math.sin(s_sat));
+		return h_sat + (h_sat > -Math.PI / 3 ? 1 : -1) * h_spin;
+	};
+	let [m1, s1, h1] = msh1;
+	let [m2, s2, h2] = msh2;
+	if (s1 > 0.05 && s2 > 0.05 && Math.abs(h1 - h2) > Math.PI / 3) {
+		const m_mid = Math.max(m1, m2, 88);
+		if (interp < 0.5) {
+			m2 = m_mid;
+			s2 = 0;
+			h2 = 0;
+			interp = interp * 2;
+		} else {
+			m1 = m_mid;
+			s1 = 0;
+			h1 = 0;
+			interp = interp * 2 - 1;
+		}
+	}
+	if (s1 < 0.05 && s2 > 0.05) {
+		h1 = adjustHue([m2, s2, h2], m1);
+	}
+	else if (s2 < 0.05 && s1 > 0.05) {
+		h2 = adjustHue([m1, s1, h1], m2);
+	}
+	const m_mid = (1 - interp) * m1 + interp * m2;
+	const s_mid = (1 - interp) * s1 + interp * s2;
+	const h_mid = (1 - interp) * h1 + interp * h2;
+	return [m_mid, s_mid, h_mid];
+}
+
+function createDivergingColorMapBlueRed(length) {
+	const start = RGB2Msh(0.230, 0.299, 0.754); // blue in msh
+	const end = RGB2Msh(0.706, 0.016, 0.150); // red in msh
+	return new Array(length).fill(null)
+		.map((_, index) => index / (length - 1))
+		.map(s => {
+			const msh = interpolateMshColor(start, end, s);
+			return [s, new THREE.Color(...Msh2RGB(...msh))];
+		})
+}
+
+function createColorMapWhiteRed(length) {
+	return new Array(length).fill(null)
+		.map((_, index) => index / (length - 1))
+		.map(s => {
+			const color = new THREE.Color(1, 1 - s, 1 - s);
+			return [s, color];
+		});
+}
+function createColorMapRainbow(length) {
+	return new Array(length).fill(null)
+		.map((_, index) => index / (length - 1))
+		.map(s => {
+			const [R, G, B] = [-1, -0.5, 0].map(d => Math.max(Math.cos((s + d) * Math.PI), 0.0));
+			const color = new THREE.Color(R, G, B);
+			return [s, color];
+		});
+}
--- final/src/main.js
+++ final/src/main.js
@@ -1,21 +1,3 @@
-function createColorMapWhiteRed(length) {
-	return new Array(length).fill(null)
-		.map((_, index) => index / (length - 1))
-		.map(s => {
-			const color = new THREE.Color(1, 1 - s, 1 - s);
-			return [s, color];
-		});
-}
-function createColorMapRainbow(length) {
-	return new Array(length).fill(null)
-		.map((_, index) => index / (length - 1))
-		.map(s => {
-			const [R, G, B] = [-1, -0.5, 0].map(d => Math.max(Math.cos((s + d) * Math.PI), 0.0));
-			const color = new THREE.Color(R, G, B);
-			return [s, color];
-		});
-}
-
 function BoundingBoxGeometry(volume) {
 	const { x: minx, y: miny, z: minz } = volume.min_coord;
 	const { x: maxx, y: maxy, z: maxz } = volume.max_coord
@@ -109,17 +91,17 @@ function TransferFunctionTexture(cmap_generator) {
 }
 
 class DatProperties {
-	first_hit_threshold = 0.5;
+	first_hit_threshold = 0.65;
 	linear_interpolation = true;
 	blinn_phong_reflection_enable = true;
 	dt = 0.5;
-	mode = 2;
+	mode = 3;
 	transfer_function_data = 0;
 	getUniformsObject() {
 		return {
 			transfer_function_data: {
 				type: "t",
-				value: TransferFunctionTexture([createColorMapWhiteRed, createColorMapRainbow][this.transfer_function_data])
+				value: TransferFunctionTexture([createDivergingColorMapBlueRed, createColorMapWhiteRed, createColorMapRainbow][this.transfer_function_data])
 			},
 			first_hit_threshold: {
 				type: "float",
@@ -173,7 +155,6 @@ function main() {
 
 	const bounding_geometry = BoundingBoxGeometry(volume);
 	const volume_texture = VolumeTexture(volume);
-	const transfer_function_texture = TransferFunctionTexture(createColorMapWhiteRed);
 
 	const bounding_material = new THREE.ShaderMaterial({
 		vertexShader: document.getElementById('bounding.vert').textContent,
@@ -203,6 +184,16 @@ function main() {
 	var raycaster_mesh = new THREE.Mesh(bounding_geometry, raycaster_material);
 	screen.scene.add(raycaster_mesh);
 
+	const cmap = createDivergingColorMapBlueRed(256);
+	// Draw color map
+	const lut = new THREE.Lut('rainbow', cmap.length);
+	lut.addColorMap('mycolormap', cmap.map(([s, c]) => [s, `0x${c.getHexString()}`]));
+	lut.changeColorMap('mycolormap');
+	screen.scene.add(lut.setLegendOn({
+		'layout': 'horizontal',
+		'position': { 'x': 100, 'y': -10, 'z': 0 },
+		'dimensions': { 'width': 4, 'height': 20 }
+	}));
 	document.addEventListener('mousemove', function () {
 		screen.light.position.copy(screen.camera.position);
 	});
@@ -227,7 +218,7 @@ function main() {
 	gui.width = 400;
 	const updateUniform = prop_name => () => raycaster_material.uniforms[prop_name].value = properties.getUniformsObject()[prop_name].value;
 	const gui_add = (prop_name, ...params) => gui.add(properties, prop_name, ...params).onChange(updateUniform(prop_name));
-	gui_add("transfer_function_data", { ["white to red"]: 0, ["rainbow colormap"]: 1 }).name("enable shader refrection");
+	gui_add("transfer_function_data", { ["white to red"]: 0, ["rainbow colormap"]: 1, ["diverging blue to red"]: 2 }).name("enable shader refrection");
 	gui_add("blinn_phong_reflection_enable").name("enable shader refrection");
 	gui_add("dt", 0.1, 1).name("sampling rate");
 	gui_add("mode", { accumulate: 0, ["raycast based isosurface"]: 1, ["X-ray"]: 2, MIP: 3 }).name("volume rendering mode");
